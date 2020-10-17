065898433feb483a49ec268c0010b72545b4df81
Crowdbotics <team@crowdbotics.com>
2020-10-17 22:52:56 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'UserProfile3147126', human_name: 'User Profile 3', access_route: 'UserProfile3147126'},
 { name: 'ArticleList4147125', human_name: 'Article List 4', access_route: 'ArticleList4147125'},
 { name: 'Maps6147123', human_name: 'Maps 6', access_route: 'Maps6147123', icon: 'map'},
-{ name: 'BlankScreen1147121', human_name: 'Blank Screen 1', access_route: 'BlankScreen1147121'},
 { name: 'BlankScreen0147120', human_name: 'Blank Screen 0', access_route: 'BlankScreen0147120'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import SignIn22147127Navigator from '../features/SignIn22147127/navigator';
 import UserProfile3147126Navigator from '../features/UserProfile3147126/navigator';
 import ArticleList4147125Navigator from '../features/ArticleList4147125/navigator';
 import Maps6147123Navigator from '../features/Maps6147123/navigator';
-import BlankScreen1147121Navigator from '../features/BlankScreen1147121/navigator';
 import BlankScreen0147120Navigator from '../features/BlankScreen0147120/navigator';
 
 /**
@@ -25,7 +24,6 @@ SignIn22147127: { screen: SignIn22147127Navigator },
 UserProfile3147126: { screen: UserProfile3147126Navigator },
 ArticleList4147125: { screen: ArticleList4147125Navigator },
 Maps6147123: { screen: Maps6147123Navigator },
-BlankScreen1147121: { screen: BlankScreen1147121Navigator },
 BlankScreen0147120: { screen: BlankScreen0147120Navigator },
 
     /** new navigators can be added here */
